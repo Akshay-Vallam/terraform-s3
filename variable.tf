e66c76e53f2b4f13120af0f1152a15bcdbec6387
Akshay-Vallam <149242737+Akshay-Vallam@users.noreply.github.com>
2023-11-25 00:13:54 +1100

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "bucket_name" {
   description = "The name of the S3 bucket to be created"
-  default     = "av-bucket" // Replace with your desired bucket name
+  default     = "av-new" // Replace with your desired bucket name
 }
 
 variable "bucket_acl" {
