c22e219846666b2582cc832f73a65d233ea74efe
Akshay-Vallam <149242737+Akshay-Vallam@users.noreply.github.com>
2023-11-24 23:39:50 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,21 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_s3_bucket" "example_bucket" {
+resource "aws_s3_bucket" "my_bucket" {
   bucket = var.bucket_name
 
   # Additional configurations if needed
   acl = var.bucket_acl
   # Other configurations like versioning, logging, encryption, etc., can be added here
+
+tags = {
+    Name = var.bucket_tag_name
+    Environment = var.bucket_tag_environment
+  }
+
+  versioning {
+    enabled = var.enable_versioning
+  }
+  
+  # Define other configurations like CORS, logging, encryption, etc., if needed
 }
