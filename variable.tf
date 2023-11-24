00e1a6bf49157e97d21758101a50d95478472bdd
Akshay-Vallam <149242737+Akshay-Vallam@users.noreply.github.com>
2023-11-25 00:09:43 +1100

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "bucket_name" {
   description = "The name of the S3 bucket to be created"
-  default     = "new-example-bucket" // Replace with your desired bucket name
+  default     = "new-terraform-bucket" // Replace with your desired bucket name
 }
 
 variable "bucket_acl" {
